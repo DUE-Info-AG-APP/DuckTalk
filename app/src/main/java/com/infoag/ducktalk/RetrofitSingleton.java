0366a17a84792268f09c38efc440ea77ae10efc5
Moritz Frankhof <frankhofmo@gmail.com>
2019-09-24 18:35:44 +0200

    make RetrofitSingleton instance final

--- app/src/main/java/com/infoag/ducktalk/RetrofitSingleton.java
+++ app/src/main/java/com/infoag/ducktalk/RetrofitSingleton.java
@@ -6,18 +6,15 @@ import retrofit2.Retrofit;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class RetrofitSingleton {
-    private static DuckTalkService instance;
+    private static final DuckTalkService instance = new Retrofit.Builder()
+            .baseUrl("ducktalk.ddns.net")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(DuckTalkService.class);
 
     private RetrofitSingleton() {}
 
-    public static synchronized DuckTalkService getInstance() {
-        if (instance == null) {
-            instance = new Retrofit.Builder()
-                    .baseUrl("ducktalk.ddns.net")
-                    .addConverterFactory(MoshiConverterFactory.create())
-                    .build()
-                    .create(DuckTalkService.class);
-        }
+    static synchronized DuckTalkService getInstance() {
         return instance;
     }
 }
